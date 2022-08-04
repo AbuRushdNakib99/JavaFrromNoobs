86c4189b6b42d7aab387953b4f745dc04caaa889
Abu Rushd Nakib <74209308+AbuRushdNakib99@users.noreply.github.com>
2022-08-04 14:16:18 +0600

    added variable basic part

---
+++ src/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+//        data_type variable_name=value
+        int number=10;
+        double d=23.45;
+        String name="Abu Rushd Nakib";
+
+        System.out.println("Number:"+number);
+        System.out.println("Double Value:"+d);
+        System.out.println("My Name is "+name);
+    }
+}
