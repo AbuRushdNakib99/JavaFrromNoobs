10d48cd1403c2b799fddf53c9c29679724483e74
Abu Rushd Nakib <74209308+AbuRushdNakib99@users.noreply.github.com>
2022-08-05 11:23:38 +0600

    Operator

---
+++ src/Basic Java/Operator.java
@@ -0,0 +1,11 @@
+public class Operator {
+    public static void main(String[] args) {
+        System.out.println("Operator");
+
+        int a=50;
+        System.out.println(a);
+        a=20;
+        a=a+50;
+        System.out.println(a);
+    }
+}
