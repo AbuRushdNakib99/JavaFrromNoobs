33245ff950d222aeca86ab9b44c2afcf8ac1dc3f
Abu Rushd Nakib <74209308+AbuRushdNakib99@users.noreply.github.com>
2022-08-05 11:10:59 +0600

    Typecasting

---
+++ src/Basic Java/TypeCasting.java
@@ -0,0 +1,21 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        System.out.println("Type Casting");
+
+//        Automatic Conversion
+
+        int i=50;
+        double d=i;
+
+        System.out.println(i);
+        System.out.println(d);
+
+//        Forceful Conversion
+        double myval=34.25;
+        int myint=(int)myval;
+
+        System.out.println(myval);
+        System.out.println(myint);
+
+    }
+}
